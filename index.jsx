8da2fff0aa318b6b1a249d6d0339f9ac99e82ad6
GregorS93 <146366523+GregorS93@users.noreply.github.com>
2025-03-20 16:39:47 +0100

    Updating naming conventions and css units

--- index.jsx
+++ index.jsx
@@ -1,4 +1,4 @@
 import ReactDOM from "react-dom/client";
-import App from "./pages/App";
+import App from "./pages/App.page";
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
